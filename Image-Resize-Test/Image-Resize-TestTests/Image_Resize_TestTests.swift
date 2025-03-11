d69d21d0dd5a171d7686b587cf5592c88dae1c3b
Lee Sejin <lsj8706@naver.com>
2025-03-11 17:49:42 +0900

    feat: 프로젝트 생성

---
+++ Image-Resize-Test/Image-Resize-TestTests/Image_Resize_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Resize_TestTests.swift
+//  Image-Resize-TestTests
+//
+//  Created by Aiden.lee on 3/11/25.
+//
+
+import Testing
+@testable import Image_Resize_Test
+
+struct Image_Resize_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
